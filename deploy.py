a3538a08a1c02ae38766ad7b4d115ab95ec0f8a4
EC2 Default User <ec2-user@ip-172-31-21-51.us-east-2.compute.internal>
2020-04-20 17:58:26 +0000

    added create update delete

--- deploy.py
+++ deploy.py
@@ -1,3 +1,19 @@
+#!/usr/bin/python3
+import getopt, sys
+
+argument_list = sys.argv[1:]
+short_options = "cduw"
+long_options = ["create", "delete", "update", "website"]
+
+try:
+    arguments, values = getopt.getopt(argument_list, short_options, long_options)
+    command = arguments[0][0]
+except getopt.error as err:
+    print (str(err))
+    sys.exit(2)
+
+################################################################################
+
 import boto3
 
 PROJECT = "upb-cloudformation"
@@ -7,9 +23,22 @@ cf = boto3.client('cloudformation')
 with open('template.yaml', 'r') as file:
     template = file.read()
 
-response = cf.create_stack(
-    StackName=PROJECT,
-    TemplateBody= template
-)
 
-print(response)
+if command in ("--create", "-c"):
+    response = cf.create_stack(
+        StackName=PROJECT,
+        TemplateBody= template
+    )
+    print(response)
+    
+if command in ("--update", "-u"):
+    response = cf.update_stack(
+        StackName=PROJECT,
+        TemplateBody= template
+    )
+    print(response)
+
+if command in ("--delete", "-d"):
+    response = cf.delete_stack(StackName=PROJECT)
+    print(response)
+
